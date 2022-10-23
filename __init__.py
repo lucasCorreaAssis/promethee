ee5bbb29acf18b666f761121cf25120ca0d32488
Lucas Correa de Assis <lucascorreadeassis@gmail.com>
2022-10-23 20:19:07 -0300

    updated module initializer

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 '''Promethee Module'''
 
-from .curves import *
+from .curves import UsualCurve, LinearCurve, UShapeCurve, VShapeCurve, LevelCurve, VShapeICurve, GaussianCurve
 from .promethee import Promethee, PrometheeOutput, Phi, Criteria
